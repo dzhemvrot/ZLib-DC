331c1af0cad240bbce875769306302f9e0858f21
Timur <78698870+dzhemvrot@users.noreply.github.com>
2022-12-28 23:36:08 +0200

    Add files via upload

--- ZLib DC.py
+++ ZLib DC.py
@@ -1,16 +1,8 @@
 import zlib
-from tkinter import *
 import tkinter as tk
-from tkinter import ttk
+from tkinter import *
 from tkinter import filedialog as fd
-from tkinter import messagebox
 from collections import deque
-
-#with open(r"C:\Users\Admin\Desktop\scene_j_a2.rsd", 'rb') as myfile:
-#    f = myfile.read()
-#z = open(r"C:\Users\Admin\Desktop\scene_j_a2.rsd.txt", "ab")
-#print("\nDecompressed String")
-#z.write(zlib.decompress(f))
 
 class Window:
 
@@ -23,15 +15,10 @@ class Window:
         self.stack = deque(maxlen = 10)
         self.stackcursor = 0
  
-        self.L1 = Label(self.Main, text = "ZLib DC - ZLib (De-)Compresser")
+        self.L1 = Label(self.Main, text = "zlib DC - zlib (De-)Compresser v3.4")
         self.L1.pack(padx = 5, pady = 0)
 
         self.menu = Menu(self.Main)
-        #self.menu.add_command(label = "Decompress", command = self.Decompress)
-        #self.menu.add_command(label = "Compress", command = self.Compress)
-        #self.menu.add_command(label = "Debug", command = self.Debug)
-        #self.menu.add_command(label = "About", command = self.About)
-        #self.menu.add_command(label = "Exit", command = root.destroy) 
          
         filemenu = Menu(self.menu, tearoff=0)
         filemenu.add_command(label="Compress", command = self.Compress, font = ("Verdana", 10))
@@ -63,11 +50,9 @@ class Window:
         self.T2 = Text(root, height=1, width = 22)
  
         self.Main.pack(padx = 15, pady = 15)
-        #self.L2.pack(padx=5, pady=5)
         self.T1.pack(padx=5, pady=5)
         self.T1.insert(tk.END, "Compression level (1-9)")
         self.T1.bind("<Button-1>", lambda args: self.T1.delete('1.0', END))
-        #self.L3.pack(padx=5, pady=5)
         self.T2.pack(padx=5, pady=5)
         self.T2.insert(tk.END, "Extension (mass only)")        
         self.T2.bind("<Button-1>", lambda args: self.T2.delete('1.0', END))
@@ -78,7 +63,6 @@ class Window:
             levelc=int(self.T1.get(1.0, "end-1c"))
         except:
             levelc=9
-        #    print("T.get failure")
 
         if levelc <= 0 and levelc != -1:
             levelc = 1
@@ -87,29 +71,28 @@ class Window:
         
         ftypes = [('All files', '*')]
         filename = fd.askopenfilename(filetypes=ftypes)
-        with open(filename, 'rb') as myfile:
-            f = myfile.read()
-        ftypes = [('All files', '*')]
-        newfilename = fd.asksaveasfilename(filetypes=ftypes, initialfile=filename)
-        z = open(newfilename, "ab")
-        print("Compressed String")
-        z.write(zlib.compress(f, level=levelc))
-        print(levelc)
-        tk.messagebox.showinfo(title='Success!', message='File compressed successfully!')
-        myfile.close()
+        if filename != "" and filename is not None:
+            with open(filename, 'rb') as myfile:
+                f = myfile.read()
+            ftypes = [('All files', '*')]
+            newfilename = fd.asksaveasfilename(filetypes=ftypes, initialfile=filename)
+            z = open(newfilename, "ab")
+            z.write(zlib.compress(f, level=levelc))
+            tk.messagebox.showinfo(title='Success!', message='File compressed successfully!')
+            myfile.close()
 
     def Decompress(self):
         ftypes = [('All files', '*')]
         filename = fd.askopenfilename(filetypes=ftypes)
-        with open(filename, 'rb') as myfile:
-            f = myfile.read()
-        ftypes = [('All files', '*')]
-        newfilename = fd.asksaveasfilename(filetypes=ftypes, initialfile=filename)
-        z = open(newfilename, "ab")
-        print("Decompressed String")
-        z.write(zlib.decompress(f))
-        tk.messagebox.showinfo(title='Success!', message='File decompressed successfully!')
-        myfile.close()
+        if filename != "" and filename is not None:
+            with open(filename, 'rb') as myfile:
+                f = myfile.read()
+            ftypes = [('All files', '*')]
+            newfilename = fd.asksaveasfilename(filetypes=ftypes, initialfile=filename)
+            z = open(newfilename, "ab")
+            z.write(zlib.decompress(f))
+            tk.messagebox.showinfo(title='Success!', message='File decompressed successfully!')
+            myfile.close()
 
     def Mdecompress(self):
         endel = None
@@ -129,40 +112,38 @@ class Window:
             
         ftypes = [('All files', '*')]
         filename = fd.askopenfilenames(filetypes=ftypes)
-        print(filename)
-        fnc = len(filename)
-        while fnc != 0:
-            print(fnc)
-            fnz = filename[fnc-1].split(',')
-            print(fnz)
-            fnz2 = fnz[0]
-            with open(fnz2, 'rb') as myfile:
-                f = myfile.read()
-            #ftypes = [('All files', '*')]
-            if endel is None or endel is not True:
-                newfilename = fnz2+endi
-            else:
-                newfilename = fnz2.replace(endi, "")
-            print(newfilename)
-            z = open(newfilename, "ab")
-            print("Decompressed String")
-            z.write(zlib.decompress(f))
-            fnc = fnc-1
-            myfile.close()
-        tk.messagebox.showinfo(title='Success!', message='Files decompressed successfully!')
+        if filename != "" and filename is not None:
+            fnc = len(filename)
+            while fnc != 0:
+                fnz = filename[fnc-1].split(',')
+                fnz2 = fnz[0]
+                with open(fnz2, 'rb') as myfile:
+                    f = myfile.read()
+                if endel is None or endel is not True:
+                    newfilename = fnz2+endi
+                else:
+                    newfilename = fnz2.replace(endi, "")
+                z = open(newfilename, "ab")
+                z.write(zlib.decompress(f))
+                fnc = fnc-1
+                myfile.close()
+            tk.messagebox.showinfo(title='Success!', message='Files decompressed successfully!')
+        else:
+            return
+
 
     def Mcompress(self):
         endel = None
         try:
             endi=self.T2.get(1.0, "end-1c")
         except:
-            endi=".zlib"
+            endi=".zl"
 
         if endi == "" or endi is None:
-            endi=".zlib"
+            endi=".zl"
 
         if endi.find(".")== -1 and not endi.startswith("-"):
-            endi=".zlib"
+            endi=".zl"
 
         if endi.startswith("-"):
             endel = True
@@ -172,7 +153,6 @@ class Window:
             levelc=int(self.T1.get(1.0, "end-1c"))
         except:
             levelc=9
-        #    print("T.get failure")
 
         if levelc <= 0 and levelc != -1:
             levelc = 1
@@ -181,41 +161,41 @@ class Window:
             
         ftypes = [('All files', '*')]
         filename = fd.askopenfilenames(filetypes=ftypes)
-        print(filename)
-        fnc = len(filename)
-        while fnc != 0:
-            print(fnc)
-            fnz = filename[fnc-1].split(',')
-            print(fnz)
-            fnz2 = fnz[0]
-            with open(fnz2, 'rb') as myfile:
-                f = myfile.read()
-            #ftypes = [('All files', '*')]
-            if endel is None or endel is not True:
-                newfilename = fnz2+endi
-            else:
-                newfilename = fnz2.replace(endi, "")
-            print(newfilename)
-            z = open(newfilename, "ab")
-            print("Compressed String")
-            z.write(zlib.compress(f, level=levelc))
-            print(levelc)
-            fnc = fnc-1
-            myfile.close()
-        tk.messagebox.showinfo(title='Success!', message='Files decompressed successfully!')
+        if filename != "" and filename is not None:
+            fnc = len(filename)
+            while fnc != 0:
+                fnz = filename[fnc-1].split(',')
+                fnz2 = fnz[0]
+                with open(fnz2, 'rb') as myfile:
+                    f = myfile.read()
+                if endel is None or endel is not True:
+                    newfilename = fnz2+endi
+                else:
+                    newfilename = fnz2.replace(endi, "")
+                z = open(newfilename, "ab")
+                z.write(zlib.compress(f, level=levelc))
+                fnc = fnc-1
+                myfile.close()
+            tk.messagebox.showinfo(title='Success!', message='Files decompressed successfully!')
+        else:
+            return
         
 
     def About(self):
-        tk.messagebox.showinfo(title='About', message='''This is a program to compress and decompress files, using Z-Library.
+        tk.messagebox.showinfo(title='About', message='''This is a program to compress and decompress files, using zlib.
 
 zlib was written by Jean-loup Gailly (compression) and Mark Adler (decompression).
 
 Program author: dzhemvrot
-Program version: 3.3
+Program version: 3.4
 Program restributed using GPL-3.0 license''')
 
 root = Tk()
 root.resizable(False, False)
 window = Window(root)
+try:
+    root.iconbitmap('icon.ico')
+except:
+    pass
 root.title(u'ZLib DC')
 root.mainloop()
